7552e3f916c0c54d78ae807d6f0a5c5025867703
Rachel Hanna <38994036+rjoyhanna@users.noreply.github.com>
2018-11-20 22:03:35 -0800

    New Site

--- petportraits/script/form.js
+++
@@ -1,8 +0,0 @@
-function addPhoto() {
-    var fileUploads = document.getElementById("fileUploads");
-    var numFiles = fileUploads.childElementCount;
-    var newNum = ((numFiles-1)/2)+1;
-    var chooseFileNum = document.createElement("span");
-    chooseFileNum.textContent = "<br>" + newNum + ".)";
-    fileUploads.appendChild(chooseFileNum);
-}
